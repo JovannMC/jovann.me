63b6c1a0156313c012bdcad9ef373bd4ac037055
JovannMC <45893380+JovannMC@users.noreply.github.com>
2024-06-25 19:03:48 +0300

    Create svelte app

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="text-center">
-		<h1 class="text-4xl font-bold">Hey!</h1>
-		<p class="text-xl mt-2">I'm <b>JovannMC</b>. I do random stuff.</p>
-	</div>
-</div>
